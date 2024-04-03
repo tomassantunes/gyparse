91604a6fd7f83c764d1f82d005e16eb25e954dcd
tomassantunes <tomasbantunes@gmail.com>
2024-04-03 17:10:43 +0100

    isKeyword and number detection implemented TODO: handle whitespaces correctly verificar o que vem a seguir do espaço indentações

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
     "strings"
+    "regexp"
 )
 
 type TokenType string
@@ -33,10 +34,14 @@ const (
     Colon TokenType = "COLON"  // ':' 
 
     // Values
-    String     TokenType = "STRING"
-    Number     TokenType = "NUMBER"
-    Bool       TokenType = "BOOL"
-    Null       TokenType = "NULL" 
+    String      TokenType = "STRING"
+    Integer     TokenType = "INTEGER"
+    Hexadecimal TokenType = "HEXADECIMAL"
+    Octal       TokenType = "OCTAL"
+    Binary      TokenType = "BINARY"
+    Float       TokenType = "FLOAT"
+    Bool        TokenType = "BOOL"
+    Null        TokenType = "NULL" 
 
     // Other
     EOF     TokenType = "EOF"
@@ -45,8 +50,13 @@ const (
 type Token struct {
     Type  TokenType
     Lexeme string
-    Literal interface{}
+    //Literal interface{}
     Line int
+}
+
+type TypeValuePair struct {
+    Type TokenType
+    Value string
 }
 
 func Lex(input string) []Token {
@@ -66,14 +76,16 @@ func Lex(input string) []Token {
         switch c {
         case '\n':
             if currLexeme.Len() > 0 {
-                tokens = append(tokens, Token{Type: String, Lexeme: cleanBuilder(&currLexeme), Line: line})
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
             }
 
             line++
         case ' ', '\r', '\t':
             // Ignore whitespace
             if currLexeme.Len() > 0 {
-                tokens = append(tokens, Token{Type: String, Lexeme: cleanBuilder(&currLexeme), Line: line})
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
             }
         case '-':
             if !hasStarted && input[start:start + 3] == "---" {
@@ -86,18 +98,118 @@ func Lex(input string) []Token {
             tokens = append(tokens, Token{Type: ListItem, Lexeme: string(c), Line: line})
         case ':':
             if currLexeme.Len() > 0 {
-                tokens = append(tokens, Token{Type: String, Lexeme: cleanBuilder(&currLexeme), Line: line})
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
             }
 
             tokens = append(tokens, Token{Type: Colon, Lexeme: string(c), Line: line})
         case '#':
             if currLexeme.Len() > 0 {
-                tokens = append(tokens, Token{Type: String, Lexeme: cleanBuilder(&currLexeme), Line: line})
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
             }
 
             for input[current + 1] != '\n' {
                 current++
             }
+        case '(':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: LeftParen, Lexeme: string(c), Line: line})
+        case ')':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: RightParen, Lexeme: string(c), Line: line})
+        case '[':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: LeftBracket, Lexeme: string(c), Line: line})
+        case ']':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: RightBracket, Lexeme: string(c), Line: line})
+        case '{':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: LeftBrace, Lexeme: string(c), Line: line})
+        case '}':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: RightBrace, Lexeme: string(c), Line: line})
+        case '&':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: Ampersand, Lexeme: string(c), Line: line})
+        case '*':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: Asterisk, Lexeme: string(c), Line: line})
+        case '!':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: Bang, Lexeme: string(c), Line: line})
+        case '|':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: VertBar, Lexeme: string(c), Line: line})
+        case '>':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: GreaterThan, Lexeme: string(c), Line: line})
+        case '`':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: SingleQuote, Lexeme: string(c), Line: line})
+        case '"':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: DoubleQuote, Lexeme: string(c), Line: line})
+        case '%':
+            if currLexeme.Len() > 0 {
+                p := cleanBuilder(&currLexeme)
+                tokens = append(tokens, Token{Type: p.Type, Lexeme: p.Value, Line: line})
+            }
+
+            tokens = append(tokens, Token{Type: Percent, Lexeme: string(c), Line: line})
         default:
             currLexeme.WriteByte(c)
         }
@@ -108,14 +220,68 @@ func Lex(input string) []Token {
     return tokens 
 }
 
-// implementar a função isKeyword (true, false, null...)
-// fazer com que cleanBuilder retorne um género de um tuple
-// com o tipo de keyword, ou tipo de dados
-// e o conteúdo em si do builder
+func isInteger(lexeme string) bool {
+	re := regexp.MustCompile(`^-?(0|[1-9]\d*)$`)
+	return re.MatchString(lexeme)
+}
 
-func cleanBuilder(sb *strings.Builder) string {
+func isHexadecimal(str string) bool {
+    re := regexp.MustCompile(`^-?0x[0-9a-fA-F]+$`)
+    return re.MatchString(str)
+}
+
+func isOctal(str string) bool {
+    re := regexp.MustCompile(`^-?0o[0-7]+$`)
+    return re.MatchString(str)
+}
+
+func isBinary(str string) bool {
+    re := regexp.MustCompile(`^-?0b[01]+$`)
+    return re.MatchString(str)
+}
+
+func isFloat(str string) bool {
+    re := regexp.MustCompile(`^-?\d+\.\d+([eE][-+]?\d+)?$|^-?\d+[eE][-+]?\d+$`)
+    return re.MatchString(str)
+}
+
+func isNumber(str string) TokenType {
+    if isInteger(str) {
+        return Integer
+    } else if isHexadecimal(str) {
+        return Hexadecimal
+    } else if isFloat(str) {
+        return Float
+    } else if isOctal(str) {
+        return Octal
+    } else if isBinary(str) {
+        return Binary
+    } 
+
+    return "nan"
+}
+
+func isKeyword(str string) TokenType {
+    switch str {
+    case "true", "false":
+        return Bool
+    case "null":
+        return Null
+    default:
+        number := isNumber(str)
+        if number != "nan" {
+            return number
+        } 
+
+        return String 
+}
+}
+
+func cleanBuilder(sb *strings.Builder) TypeValuePair {
     content := sb.String()
     sb.Reset()
 
-    return content
+    tokenType := isKeyword(content)
+
+    return TypeValuePair{Type: tokenType, Value: content}
 }
